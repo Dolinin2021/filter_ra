940117887d89b9a4604f8630bfb8ad419c5cd28e
Dolinin2021 <programmistdolinin@mail.ru>
2023-11-15 00:29:40 +0600

    edit config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://dolinin2021.github.io/filter_ra/",
   plugins: [react()],
 })
